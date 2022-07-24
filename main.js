56d269b10a046f9de6a3a0942248502785d9fcaf
umid2002 <umidhusanov03@bk.ru>
2022-07-24 17:27:19 +0500

    bug fixed

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ const menuListElement = document.querySelector('.menu-list');
 hamburgerElement.addEventListener('click', ()=>{
   menuListElement.classList.toggle('hide-list')
 })
+
+window.addEventListener("scroll", () => {
+  menuListElement.classList.add('hide-list')
+})
+
+
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
